4f7da3988b263681e89b3d46d070bf0931558520
Johnny L <februar2@live.no>
2016-10-23 20:25:30 +0200

    make tran layout

--- BankingStuff/Models/Account.cs
+++ BankingStuff/Models/Account.cs
@@ -1,6 +1,7 @@
 ﻿using BankingStuff.DataAccessLayer;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -12,15 +13,16 @@ namespace BankingStuff.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int accountID { get; set; }
         public string accountName { get; set; }
+        [RegularExpression(@"[0-9]{11}", ErrorMessage = "Please write a correct person number")]
         public long customerID { get; set; }
         public int balance { get; set; }
 
-        public string getAccount(int customerID)
+        public string getAccount(long customerID)
         {
             BankContext db = new BankContext();
             string name = "";
             name += db.Customers.First(x => x.customerID == customerID).firstName;
-            name += " " + db.Customers.First(x => x.customerID == customerID).firstName;
+            name += " " + db.Customers.First(x => x.customerID == customerID).lastName;
 
             return name;
         }
